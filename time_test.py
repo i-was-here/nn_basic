c804bd69febfbee14cea7830e02c9ffd17a619fb
i-was-here <nirmitarora26@gmail.com>
2024-02-27 11:53:19 +0530

    script for testing the processor time by MLP module built on each of the row Vector and Value classes

---
+++ time_test.py
@@ -0,0 +1,112 @@
+import time
+
+def vector_test(num_exp=100):
+    from nn_row import MLP
+    from optimizers import GD
+    from losses_row import mse_loss
+    from grad_engine_row import draw_dot, Vector
+    import numpy as np
+    import random
+    random.seed(42)
+
+    # hyper params initialization
+    epochs = 20
+    lr = 0.01
+
+    # data initialization
+    x = [
+        Vector(np.array([3.0, 4.0, 5.0])),
+        Vector(np.array([-5.0, -2.0, 8.0])),
+        Vector(np.array([-2.0, 2.5, -4.5])),
+        Vector(np.array([2.0, -1.5, -4.0]))
+    ]
+    y_true = [Vector(np.array([1])), Vector(np.array([-1])), Vector(np.array([-1])), Vector(np.array([1]))]
+    
+    start = time.perf_counter()
+    for experiment in range(num_exp):
+
+        # model initialization
+        model = MLP(3, [6, 6, 6, 1])
+        optim = GD(model.parameters(), lr)
+
+        # training loop
+        for epoch in range(epochs):
+
+            # forward pass / inference
+            y_pred = [model(inp) for inp in x]
+            loss_val = mse_loss(y_pred, y_true)
+            # print(epoch+1, loss_val.value)
+
+            # zero grad
+            optim.zero_grad()
+            
+            # backward pass
+            loss_val.backward()
+
+            # update params
+            optim.step()
+
+
+        # # saving graph of nn as image
+        # graph = draw_dot(loss_val)
+        # graph.render("nn", format="png", cleanup=True)
+    stop = time.perf_counter()
+    print("Average Vector way time: ", (stop-start)/num_exp)
+
+
+def value_test(num_exp=100):
+    from nn import MLP
+    from optimizers import GD
+    from losses import mse_loss
+    from grad_engine import draw_dot
+    import random
+    random.seed(42)
+
+    # hyper params initialization
+    epochs = 20
+    lr = 0.01
+
+    # data initialization
+    x = [
+        [3.0, 4.0, 5.0],
+        [-5.0, -2.0, 8.0],
+        [-2.0, 2.5, -4.5],
+        [2.0, -1.5, -4.0]
+    ]
+    y_true = [1, -1, -1, 1]
+
+    start = time.perf_counter()
+    for experiment in range(num_exp):
+        
+        # model initialization
+        model = MLP(3, [6, 6, 6, 1])
+        optim = GD(model.parameters(), lr)
+
+        # training loop
+        for epoch in range(epochs):
+
+            # forward pass / inference
+            y_pred = [model(inp) for inp in x]
+            loss_val = mse_loss(y_pred, y_true)
+            # print(epoch+1, loss_val.value)
+
+            # zero grad
+            optim.zero_grad()
+            
+            # backward pass
+            loss_val.backward()
+
+            # update params
+            optim.step()
+
+
+        # # saving graph of nn as image
+        # graph = draw_dot(loss_val)
+        # graph.render("nn", format="png", cleanup=True)
+    stop = time.perf_counter()
+    print("Average Value way time: ", (stop-start)/num_exp)
+
+if __name__=="__main__":
+    vector_test(100)
+    print()
+    value_test(100)
